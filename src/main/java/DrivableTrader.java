d4b95294e579c640dd3b91063afae2d8ceb0a102
Charlie Tao <charlietaoxl@gmail.com>
2022-10-09 21:27:37 -0400

    Implemented DrivableMap, DrivableTrader, and Trader classes.

--- src/main/java/DrivableTrader.java
+++ src/main/java/DrivableTrader.java
@@ -1,4 +1,4 @@
-/* TODO: Create a subclass of Trader named DrivableTrader
+/* Create a subclass of Trader named DrivableTrader
  * This class should be identical to Trader, except that it takes
  * only Drivable objects in its inventory, wishlist, etc.
  *
@@ -10,4 +10,44 @@
  * Look at DomesticatableTrader.java for an example.
  */
 
+import java.sql.DriverAction;
 import java.util.List;
+
+public class DrivableTrader extends Trader<Drivable>{
+    /**
+     * Construct a DrivableTrader, giving them the given inventory,
+     * wishlist, and money.
+     *
+     * @param inventory Objects in this Trader's inventory
+     * @param wishlist  Objects in this Trader's wishlist
+     * @param money     The Trader's money
+     */
+    public DrivableTrader(List<Drivable> inventory, List<Drivable> wishlist,
+                          int money) {
+        super(inventory, wishlist, money);
+    }
+
+    /**
+     * Construct a DrivableTrader, giving them the
+     * given money and an empty wishlist and inventory.
+     *
+     * @param money     The Trader's money
+     */
+    public DrivableTrader(int money) {
+        super(money);
+    }
+
+    @Override
+    public void addToWishlist(Drivable item) {
+        super.addToWishlist(item);
+    }
+
+    @Override
+    public int getSellingPrice(Drivable item) {
+        int selling_price = super.getSellingPrice(item);
+        if (selling_price == Tradable.MISSING_PRICE){
+            return selling_price;
+        }
+        return selling_price + item.getMaxSpeed();
+    }
+}
